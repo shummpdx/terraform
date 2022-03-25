37c268dfd48f2a7dfaa6d05fca25a406edea86f6
Sean Humm <hummsean@pdx.edu>
2022-03-24 17:40:22 -0700

    launchConfiguration

---
+++ launchConfiguration/main.tf
@@ -0,0 +1,83 @@
+# VPC Practice
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.4"
+    }
+    tls = {
+      source = "hashicorp/tls"
+      version = "3.1"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+# Allow SSH access
+resource "aws_security_group" "sshSecurity" {
+  name = "sshSecurity"
+  description = "Allow SSH"
+  
+  ingress {
+    description = "SSH"
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "sshSecurity"
+  }
+}
+
+# Allow HTTP access
+resource "aws_security_group" "httpSecurity" {
+  name = "httpSecurity"
+  description = "Zodiark Allows HTTP"
+
+  ingress {
+    description = "HTTP"
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "httpSecurity"
+  }
+}
+
+# Allow traffic into the VPC
+resource "aws_security_group" "outboundTraffic" {
+  name = "outboundTraffic"
+  description = "Allow for outbound traffic"
+
+  egress {
+    description = "outbound traffic"
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_launch_configuration" "as_conf" {
+  name = "web_config"
+  image_id = "ami-04cb38c0c7b926017"
+  instance_type = "t2.micro"
+  security_groups = [
+          "${aws_security_group.sshSecurity.id}",
+          "${aws_security_group.httpSecurity.id}",
+          "${aws_security_group.outboundTraffic.id}"
+  ]
+  key_name = "CAA"
+  ebs_block_device {
+    device_name = "Encrypted"
+    snapshot_id = "snap-02912cea220ccc29d"
+  }
+}
