f4ceaeb9d7ced8f64b22c6fd2aa2b907c741927e
Sean Humm <hummsean@pdx.edu>
2022-04-24 13:23:51 -0700

    count - create two instances/output both ips

---
+++ resourceMetaArgs/count/main.tf
@@ -0,0 +1,27 @@
+# VPC Practice
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.4"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_instance" "my_server" {
+  count = 2
+  ami = "ami-0359b3157f016ae46"
+  instance_type = "t2.micro"
+  
+  tags = {
+    Name = "Server-${count.index}"
+  }
+}
+
+output "public_ips" {
+  value = aws_instance.my_server.*.public_ip
+}
