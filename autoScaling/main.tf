2665dfe8f1d15c3b0b60267ef59a1596f58c44ad
Sean Humm <hummsean@pdx.edu>
2022-05-10 18:33:04 -0700

    WIP: autoscaling policy for 50% CPUUTIL

--- autoScaling/main.tf
+++ autoScaling/main.tf
@@ -34,24 +34,6 @@ resource "aws_security_group" "sshSecurity" {
   }
 }
 
-# Allow HTTP access
-resource "aws_security_group" "httpSecurity" {
-  name = "httpSecurity"
-  description = "Zodiark Allows HTTP"
-
-  ingress {
-    description = "HTTP"
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "httpSecurity"
-  }
-}
-
 # Allow traffic into the VPC
 resource "aws_security_group" "outboundTraffic" {
   name = "outboundTraffic"
@@ -68,32 +50,46 @@ resource "aws_security_group" "outboundTraffic" {
 
 resource "aws_launch_configuration" "as_conf" {
   name = "web_config"
-  image_id = "ami-06ef8e22557d9ec79"
-  instance_type = "c4.large"
+  image_id = "ami-0ca285d4c2cda3300"
+  instance_type = "t2.micro"
   security_groups = [
           "${aws_security_group.sshSecurity.id}",
-          "${aws_security_group.httpSecurity.id}",
           "${aws_security_group.outboundTraffic.id}"
   ]
   key_name = "ec2Key"
 }
 
-
-resource "aws_placement_group" "myCluster" {
+/*resource "aws_placement_group" "myCluster" {
   name = "Custard the Clustar"
   strategy = "cluster"
-}
+}*/
 
 resource "aws_autoscaling_group" "bar" {
   name = "My AutoBot"
   availability_zones = ["us-west-2a"]
-  max_size = 2 
-  min_size = 1
+  max_size = 3 
+  min_size = 2
   health_check_grace_period = 300
-  health_check_type = "ELB"
-  desired_capacity = 1 
+  health_check_type = "EC2"
+  desired_capacity = 2 
   force_delete = true
-  placement_group = aws_placement_group.myCluster.id
+  //placement_group = aws_placement_group.myCluster.id
   launch_configuration = aws_launch_configuration.as_conf.id
+}
 
+resource "aws_autoscaling_policy" "CPU_util" {
+  name = "CPU_Util"
+  autoscaling_group_name = aws_autoscaling_group.bar.name
+  adjustment_type = "ChangeInCapacity"
+  policy_type = "PredictiveScaling"
+  predictive_scaling_configuration {
+    metric_specification {
+      target_value = 10
+
+      predefined_load_metric_specification {
+        predefined_metric_type = "ASGTotalCPUUtilization"
+        resource_label = "testLabel"
+      }
+    }
+  }
 }
