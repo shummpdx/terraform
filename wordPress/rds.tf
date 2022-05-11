e27e72c2a1da08464a71827d687726abf2da264e
Sean Humm <hummsean@pdx.edu>
2022-05-10 18:32:05 -0700

    changed to more generic password/include design

--- wordPress/rds.tf
+++ wordPress/rds.tf
@@ -5,7 +5,7 @@ resource "aws_db_instance" "wordPress" {
     allocated_storage = 20
     db_name = "wordpress"
     username = "admin"
-    password = "sW^TxU6R"
+    password = "password"
     skip_final_snapshot = false
     db_subnet_group_name = aws_db_subnet_group.privateSubnets.id 
     vpc_security_group_ids = [aws_security_group.rds_security.id]
