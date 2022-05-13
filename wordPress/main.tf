7caee59f814882a55a01dde76a7259061f50b668
Sean Humm <sean.humm@gmail.com>
2022-05-13 12:46:24 -0700

    remove datasource

--- wordPress/main.tf
+++ wordPress/main.tf
@@ -1,8 +1,3 @@
-# Assign the private key that was created on my local computer
-data "tls_public_key" "example" {
-  private_key_pem = "${file(var.PRIVATE_KEY_PATH)}"
-}
-
 # Deploy(?) the key pay
 resource "aws_key_pair" "deployer" {
   key_name = "ec2Key"
