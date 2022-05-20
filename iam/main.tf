88c901fd75a352dd0878445c9e303a28f5a17474
Sean Humm <hummsean@pdx.edu>
2022-05-19 18:10:59 -0700

    cidr_block fix

--- iam/main.tf
+++ iam/main.tf
@@ -3,13 +3,13 @@ provider "aws" {
 }
 
 resource "aws_vpc" "development_VPC" {
-    cidr_block = "10.0.0.0/16"
+    cidr_block = "10.2.0.0/16"
 }
 
 module "instanceGenerator" {
     source = "../modules/instances"
     vpc_id = aws_vpc.development_VPC.id
-    cidr_block = "10.0.0.0/16"
+    cidr_block = "10.2.0.0/16"
     name = "Development"
     instance_ami = "ami-0ee8244746ec5d6d4"
 }
