24d3a304f617b87f539243a9d0003757d2d08694
Sean Humm <hummsean@pdx.edu>
2022-05-19 18:04:27 -0700

    Begin IAM build for Session Manager

---
+++ iam/main.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+    region = "us-west-2"
+}
+
+resource "aws_vpc" "development_VPC" {
+    cidr_block = "10.0.0.0/16"
+}
+
+module "instanceGenerator" {
+    source = "../modules/instances"
+    vpc_id = aws_vpc.development_VPC.id
+    cidr_block = "10.0.0.0/16"
+    name = "Development"
+    instance_ami = "ami-0ee8244746ec5d6d4"
+}
