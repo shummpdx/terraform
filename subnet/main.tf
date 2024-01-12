6810626f332f2951daf5861463e73537bde05c72
Sean Humm <sean.humm@gmail.com>
2024-01-12 15:04:23 -0800

    subnet test

---
+++ subnet/main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+        aws = {
+        source  = "hashicorp/aws"
+        version = "5.31"
+        }
+    }
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_vpc" "main" {
+    cidr_block = "10.0.0.0/16"
+    tags = {
+        Name = "main"
+    }
+}
+
+resource "aws_subnet" "main" {
+    vpc_id = aws_vpc.main.id
+    cidr_block = "10.0.1.0/24"
+
+    tags = {
+        Name = "Main"
+    }
+}
