f38312e0ea767fd4904bb2c76ab2d80afaab2649
shummpdx <sean.humm@gmail.com>
2024-01-16 14:04:01 -0800

    Issues getting subnet/secruity groups/gateway associated correctly

--- launchTemplate/main.tf
+++ launchTemplate/main.tf
@@ -7,20 +7,68 @@ terraform {
   }
 }
 
-resource "aws_launch_template" "test" {
-    image_id = "ami-0944e91aed79c721c"
-    instance_type = "t2.micro"
-    key_name = "windows_keypair"
-
-    network_interfaces {
-        associate_public_ip_address = true
+resource "aws_vpc" "ltVPC" {
+    cidr_block = "10.0.0.0/16"
+    tags = {
+        Name = "ltVPC"
     }
+}
 
-    placement {
-      availability_zone = "us-west-2a"
-    }
+resource "aws_subnet" "ltSub" {
+    vpc_id = aws_vpc.ltVPC.id
+    cidr_block = "10.0.1.0/24"
 
     tags = {
-      Name = "Terraform Test"
+        Name = "ltSub"
     }
+}
+
+resource "aws_security_group" "sshSecurity" {
+  name = "sshSecurity"
+  description = "Allow SSH"
+  vpc_id = aws_vpc.ltVPC.id
+
+  ingress {
+    description = "SSH"
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+      Name = "sshSecurity"
+  }
+}
+
+resource "aws_vpc_endpoint" "ec2" {
+  vpc_id = aws_vpc.ltVPC.id
+  
+}
+
+resource "aws_vpc_endpoint_subnet_association" "endpointSubnet" {
+  vpc_endpoint_id = aws_vpc.ltVPC.id
+  subnet_id = aws_subnet.ltSub.id
+}
+
+resource "aws_launch_template" "test" {
+  image_id = "ami-0944e91aed79c721c"
+  instance_type = "t2.micro"
+  key_name = "windows_keypair"
+
+  network_interfaces {
+    associate_public_ip_address = true
+    security_groups = [aws_security_group.sshSecurity.id]
+    subnet_id = aws_subnet.ltSub.id
+  }
+
+  placement {
+    availability_zone = aws_subnet.ltSub.availability_zone
+  }
+
+  #vpc_security_group_ids = [aws_security_group.sshSecurity.id]
+
+  tags = {
+    Name = "Terraform Test"
+  }
 }
